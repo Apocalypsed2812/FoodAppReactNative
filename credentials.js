5bc390e6dad5dd7495efa736c5ae242f32ad985b
Apocalysed2812 <tien.pham@omn1solution.com>
2025-11-14 20:29:44 +0700

    edit api

--- credentials.js
+++ credentials.js
@@ -3,7 +3,9 @@ export const TOKEN_NAME = "token_user_vt_ecommerce_name"
 //export const SERVER_API = "https://nguyen-vinh-tieng-ecommerce.herokuapp.com/api/"
 //export const SERVER_API = "http://localhost:5000/api/"
 // export const SERVER_API = "https://food-shop-api.herokuapp.com/api/"
-export const SERVER_API = 'https://food-app-api.vercel.app/api/';
+// export const SERVER_API = 'https://food-app-api.vercel.app/api/';
 // export const SERVER_API = 'https://elated-actually-sodalite.glitch.me/api/';
+export const SERVER_API = 'https://tiki-react-api.onrender.com/api/';
+
 
 
